a0ba7c4947291dd75a951dd799b8d1e8a660330a
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-04-21 14:23:27 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -9,7 +9,7 @@
 
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
-console.log('---TASK 2---');
+
 
 class Storage {
     #items;
@@ -33,11 +33,11 @@ class Storage {
         }
     }
 
-}
+};
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 
-
+console.log('---TASK 2---');
 
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
